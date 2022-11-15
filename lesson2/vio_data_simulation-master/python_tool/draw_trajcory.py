7294e25e1425f8e42b24896eac2192dfdb7c32c1
trebladev <2253714301@qq.com>
2022-11-15 09:12:07 +0800

    fix python file bug

--- lesson2/vio_data_simulation-master/python_tool/draw_trajcory.py
+++ lesson2/vio_data_simulation-master/python_tool/draw_trajcory.py
@@ -30,9 +30,9 @@ with open(filepath + '/imu_pose.txt', 'r') as f:  # imu_circle   imu_spline
     data = f.readlines()  # txt中所有字符串读入data
     for line in data:
         odom = line.split()  # 将单个数据分隔开存好
-        numbers_float = map(float, odom)  # 转化为浮点数
+        numbers_float = list(map(float, odom))  # 转化为浮点数
 
-        position.append([numbers_float[tx_index], numbers_float[tx_index + 1], numbers_float[tx_index + 2]])
+        position.append(numbers_float[tx_index:tx_index+3])
 
 position1 = []
 quaterntions1 = []
@@ -42,11 +42,11 @@ with open(filepath + '/imu_int_pose.txt', 'r') as f:  # imu_pose   imu_spline
     data = f.readlines()  # txt中所有字符串读入data
     for line in data:
         odom = line.split()  # 将单个数据分隔开存好
-        numbers_float = map(float, odom)  # 转化为浮点数
+        numbers_float = list(map(float, odom))  # 转化为浮点数
 
         # timestamp.append( numbers_float[0])
         # quaterntions1.append( [numbers_float[tx_index+6], numbers_float[tx_index+3],numbers_float[tx_index+4],numbers_float[tx_index+5]   ] )   # qw,qx,qy,qz
-        position1.append([numbers_float[tx_index], numbers_float[tx_index + 1], numbers_float[tx_index + 2]])
+        position1.append(numbers_float[tx_index:tx_index+3])
 
 position2 = []
 quaterntions2 = []
@@ -56,19 +56,19 @@ with open(filepath + '/imu_int_pose_noise.txt', 'r') as f:  # cam_pose_opt_o_0  
     data = f.readlines()  # txt中所有字符串读入data
     for line in data:
         odom = line.split()  # 将单个数据分隔开存好
-        numbers_float = map(float, odom)  # 转化为浮点数
+        numbers_float = list(map(float, odom))  # 转化为浮点数
 
         # timestamp.append( numbers_float[0])
         # quaterntions2.append( [numbers_float[tx_index+6], numbers_float[tx_index+3],numbers_float[tx_index+4],numbers_float[tx_index+5]   ] )   # qw,qx,qy,qz
-        position2.append([numbers_float[tx_index], numbers_float[tx_index + 1], numbers_float[tx_index + 2]])
+        position2.append(numbers_float[tx_index:tx_index+3])
 
     ### plot 3d
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(projection='3d')
 
-xyz = zip(*position)
-xyz1 = zip(*position1)
-xyz2 = zip(*position2)
+xyz = list(zip(*position))
+xyz1 = list(zip(*position1))
+xyz2 = list(zip(*position2))
 print
 ax.plot(xyz[0], xyz[1], xyz[2], label='gt')
 ax.plot(xyz1[0], xyz1[1], xyz1[2], label='imu_int')
